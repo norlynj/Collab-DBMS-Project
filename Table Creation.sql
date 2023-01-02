dc36fe3aa3957958d62d2ac61e79b5d6d6419f13
Norlyn Jane <nacastillo2@up.edu.ph>
2023-01-02 09:14:57 +0800

    create table schema with constraints

--- Table Creation.sql
+++ Table Creation.sql
@@ -1,5 +1,18 @@
+drop database if exists collab;
 create database collab;
 use collab;
+
+drop table if exists student;
+drop table if exists role;
+drop table if exists task;
+drop table if exists status;
+drop table if exists label;
+drop table if exists comment;
+drop table if exists assign_label;
+drop table if exists assign_task;
+drop table if exists task_status;
+drop table if exists task_label;
+drop table if exists task_comment;
 
 create table student(
 id varchar(10) primary key,
@@ -22,7 +35,7 @@ tag varchar(20) unique not null
 create table assign_label(
 student_id varchar(10) primary key,
 label_id varchar(10),
-foreign key (student_id) references student(id),
+foreign key (student_id) references student(id) on delete cascade,
 foreign key (label_id) references label(id)
 );
 
@@ -57,29 +70,29 @@ foreign key (role_id) references role(id)
 create table assign_task(
 student_id varchar(10),
 task_id varchar(10) primary key,
-foreign key (student_id) references student(id),
-foreign key (task_id) references task(id)
+foreign key (student_id) references student(id) on delete cascade,
+foreign key (task_id) references task(id) on delete cascade
 );
 
 create table task_label(
 task_id varchar(10) primary key,
 label_id varchar(10),
-foreign key (task_id) references task(id),
-foreign key (label_id) references label(id)
+foreign key (task_id) references task(id) on delete cascade,
+foreign key (label_id) references label(id) on delete cascade
 );
 
 create table task_status(
 task_id varchar(10) primary key,
 status_id varchar(10),
-foreign key (task_id) references task(id),
-foreign key (status_id) references status(id)
+foreign key (task_id) references task(id) on delete cascade,
+foreign key (status_id) references status(id) on delete cascade
 );
 
 create table task_comment(
 task_id varchar(10) primary key,
 comment_id varchar(10),
 student_id varchar(10),
-foreign key (task_id) references task(id),
-foreign key (comment_id) references comment(id),
-foreign key (student_id) references student(id)
+foreign key (task_id) references task(id) on delete cascade,
+foreign key (comment_id) references comment(id) on delete cascade,
+foreign key (student_id) references student(id) on delete cascade
 );
